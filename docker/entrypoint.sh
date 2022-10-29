6351cafb03f3d97be74144f3bbcf74e7728f882d
Sebastian Castro <sebas.a.castro@gmail.com>
2022-10-29 12:48:33 -0400

    Switch to apt installs of turtlebot3 packages

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,8 +19,8 @@ fi
 
 # Set environment variables
 export TURTLEBOT3_MODEL=waffle_pi
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix tb3_worlds)/share/tb3_worlds/models
-# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/humble/share/turtlebot3_gazebo/models
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 # Execute the command passed into this entrypoint
 exec "$@"
