17ccfbaf7d4d88adedbe78a6c539aba6025be470
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2024-12-09 16:11:19 +0200

    Update to new Gazebo

--- tb3_worlds/launch/tb3_world.launch.py
+++ tb3_worlds/launch/tb3_world.launch.py
@@ -1,58 +1,161 @@
-from os.path import join
+# Copyright (C) 2023 Open Source Robotics Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This is all-in-one launch script intended for use by nav2 developers."""
+
+import os
+import tempfile
 
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+    RegisterEventHandler,
+)
+from launch.conditions import IfCondition
+from launch.event_handlers import OnShutdown
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    tb3_gazebo_dir = get_package_share_directory("turtlebot3_gazebo")
-    gazebo_ros_dir = get_package_share_directory("gazebo_ros")
+    # Get the launch directory
+    sim_dir = get_package_share_directory('nav2_minimal_tb3_sim')
 
-    use_sim_time = LaunchConfiguration("use_sim_time", default="true")
-    x_pose = LaunchConfiguration("x_pose", default="0.0")
-    y_pose = LaunchConfiguration("y_pose", default="0.0")
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-    tb3_world_dir = get_package_share_directory("tb3_worlds")
-    default_map = join(tb3_world_dir, "maps", "sim_house_map.yaml")
-    default_world = join(tb3_world_dir, "worlds", "sim_house.world")
+    # Launch configuration variables specific to simulation
+    world = LaunchConfiguration('world')
+    pose = {
+        'x': LaunchConfiguration('x_pose', default='0.0'),
+        'y': LaunchConfiguration('y_pose', default='0.0'),
+        'z': LaunchConfiguration('z_pose', default='0.01'),
+        'R': LaunchConfiguration('roll', default='0.00'),
+        'P': LaunchConfiguration('pitch', default='0.00'),
+        'Y': LaunchConfiguration('yaw', default='0.00'),
+    }
+    robot_name = LaunchConfiguration('robot_name')
+    robot_sdf = LaunchConfiguration('robot_sdf')
 
-    # Start Gazebo server and client
-    gzserver_cmd = IncludeLaunchDescription(
+    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace', default_value='', description='Top-level namespace'
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true',
+    )
+
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(get_package_share_directory('tb3_worlds'), 'worlds', 'sim_house.world'),
+        description='Full path to world model file to load',
+    )
+
+    declare_robot_name_cmd = DeclareLaunchArgument(
+        'robot_name', default_value='turtlebot3_waffle', description='name of the robot'
+    )
+
+    declare_robot_sdf_cmd = DeclareLaunchArgument(
+        'robot_sdf',
+        default_value=os.path.join(get_package_share_directory('tb3_worlds'), 'urdf', 'gz_waffle.sdf.xacro'),
+        description='Full path to robot sdf file to spawn the robot in gazebo',
+    )
+
+    urdf = os.path.join(sim_dir, 'urdf', 'turtlebot3_waffle.urdf')
+    with open(urdf, 'r') as infp:
+        robot_description = infp.read()
+
+    robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace=namespace,
+        output='screen',
+        parameters=[
+            {'use_sim_time': use_sim_time, 'robot_description': robot_description}
+        ],
+        remappings=remappings,
+    )
+
+
+    # The SDF file for the world is a xacro file because we wanted to
+    # conditionally load the SceneBroadcaster plugin based on wheter we're
+    # running in headless mode. But currently, the Gazebo command line doesn't
+    # take SDF strings for worlds, so the output of xacro needs to be saved into
+    # a temporary file and passed to Gazebo.
+    world_sdf = tempfile.mktemp(prefix='tb3_', suffix='.sdf')
+    world_sdf_xacro = ExecuteProcess(
+        cmd=['xacro', '-o', world_sdf, world])
+    gazebo_server = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', '-s', world_sdf],
+        output='screen',
+    )
+
+    remove_temp_sdf_file = RegisterEventHandler(event_handler=OnShutdown(
+        on_shutdown=[
+            OpaqueFunction(function=lambda _: os.remove(world_sdf))
+        ]))
+
+    gazebo_client = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            join(gazebo_ros_dir, "launch", "gzserver.launch.py")
+            os.path.join(get_package_share_directory('ros_gz_sim'),
+                         'launch',
+                         'gz_sim.launch.py')
         ),
-        launch_arguments={"world": default_world}.items(),
-    )
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            join(gazebo_ros_dir, "launch", "gzclient.launch.py")
-        )
+        launch_arguments={'gz_args': ['-v4 -g ']}.items(),
     )
 
-    # Start robot state publisher
-    robot_state_publisher_cmd = IncludeLaunchDescription(
+    gz_robot = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            join(tb3_gazebo_dir, "launch", "robot_state_publisher.launch.py")
-        ),
-        launch_arguments={"use_sim_time": use_sim_time}.items(),
-    )
+            os.path.join(sim_dir, 'launch', 'spawn_tb3.launch.py')),
+        launch_arguments={'namespace': namespace,
+                          'use_sim_time': use_sim_time,
+                          'robot_name': robot_name,
+                          'robot_sdf': robot_sdf,
+                          'x_pose': pose['x'],
+                          'y_pose': pose['y'],
+                          'z_pose': pose['z'],
+                          'roll': pose['R'],
+                          'pitch': pose['P'],
+                          'yaw': pose['Y']}.items())
 
-    # Spawn the turtlebot
-    spawn_turtlebot_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            join(tb3_gazebo_dir, "launch", "spawn_turtlebot3.launch.py")
-        ),
-        launch_arguments={"x_pose": x_pose, "y_pose": y_pose}.items(),
-    )
-
+    # Create the launch description
     return LaunchDescription(
         [
-            gzserver_cmd,
-            gzclient_cmd,
-            robot_state_publisher_cmd,
-            spawn_turtlebot_cmd,
+            declare_namespace_cmd,
+            declare_use_sim_time_cmd,
+            declare_world_cmd,
+            declare_robot_name_cmd,
+            declare_robot_sdf_cmd,
+            world_sdf_xacro,
+            remove_temp_sdf_file,
+            gz_robot,
+            gazebo_server,
+            gazebo_client,
+            robot_state_publisher_cmd 
         ]
     )
