b5bd7d1ff2cafc0997a880ac243817ef26f5463d
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2024-12-10 18:55:38 +0200

    updating origin

--- tb3_worlds/launch/tb3_world.launch.py
+++ tb3_worlds/launch/tb3_world.launch.py
@@ -1,19 +1,5 @@
-# Copyright (C) 2023 Open Source Robotics Foundation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""This is all-in-one launch script intended for use by nav2 developers."""
-
+# This is a modified copy of https://github.com/ros-navigation/navigation2/blob/d6b02ac407a26d9394a23f76620f25f2274c8c37/nav2_bringup/launch/tb3_simulation_launch.py
+# Launches robot_state_publiser and gazebo with world argument
 import os
 import tempfile
 
@@ -27,10 +13,9 @@ from launch.actions import (
     OpaqueFunction,
     RegisterEventHandler,
 )
-from launch.conditions import IfCondition
 from launch.event_handlers import OnShutdown
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
